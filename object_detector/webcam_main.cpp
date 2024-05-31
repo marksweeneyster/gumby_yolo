a59878699cca505a3a5f5a646c8874bc476e69cb
marksweeneyster <marksweeneyster@gmail.com>
2024-05-30 19:44:40 -0600

    GNU licensing boilerplate

--- object_detector/webcam_main.cpp
+++ object_detector/webcam_main.cpp
@@ -1,6 +1,19 @@
+//This file is part of gumby_yolo, an object detector that works with various cameras.
+// Copyright (C) 2024 Mark Sweeney, marksweeneyster@gmail.com
 //
-// Created by msweeney on 10/19/2022.
+// gumby_yolo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 #include <opencv2/dnn.hpp>
 #include <opencv2/imgproc.hpp>
